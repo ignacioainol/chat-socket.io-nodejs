e26d23b0906199addc46d665cb0da8f972a5eb7d
Ignacio <shoik.xtazy@gmail.com>
2019-07-02 17:20:15 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//static files
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',(req,res) => {
+    res.render('index.html');
+})
+
+const server = app.listen(app.get('port'),() =>{
+    console.log("server on port", app.get('port'));
+});
+
+const SocketIO = require('socket.io');
+const io = SocketIO.listen(server); 
+
+//websocket
+io.on('connection',(socket) =>{
+    console.log("new conection :D!", socket.id);
+
+    socket.on('chat:message',(data) => {
+        io.sockets.emit('chat:message',data);
+    });
+});
